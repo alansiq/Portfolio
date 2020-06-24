b56e95871799b478ae10b94583e094ab9c5ba243
Alan Siqueira <alan.siqueira@outlook.com>
2020-06-24 20:11:09 -0300

    Created TsParticles component for '/' and implemented it

---
+++ src/pages/Test/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SkyParticles from "../../components/SkyParticles";
+
+
+export default function Test() {
+  return (
+
+      <>
+
+      <SkyParticles>
+      <h1>Hello world, this is a test page</h1>
+      <h2>teste</h2>
+      </SkyParticles>
+      </>
+  );
+}
